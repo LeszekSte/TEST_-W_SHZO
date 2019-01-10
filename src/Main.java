fe24709c6dd08b1d4c1c347f37e404860d170bff
LeszekS <leszekstepien@o2.pl>
2019-01-10 13:21:40 +0100

    Dopisanie pliku .iml

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Drukowanie na śniadanie");
+        System.out.println("Drukowanie na śniadanie - bułeczki");
     }
 }
